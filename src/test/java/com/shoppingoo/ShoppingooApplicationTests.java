551ccadc731b9bd0be21de680eda6a34169094ee
thvldk1654 <thvldk1654@naver.com>
2024-10-23 13:40:52 +0900

    user

---
+++ src/test/java/com/shoppingoo/ShoppingooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
